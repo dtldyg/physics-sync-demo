06f3290ea7385f316803e9ba3f2d18b568fd76bc
liyiyue <liyiyue@hypergryph.com>
2021-02-23 16:42:29 +0800

    暂存

--- src/logic/system_dead_reckoning.py
+++ src/logic/system_dead_reckoning.py
@@ -8,5 +8,9 @@ class SystemDeadReckoning(ecs.System):
 		super(SystemDeadReckoning, self).__init__((ecs.LABEL_TRANSFORM,))
 
 	def update(self, dt, component_tuples):
+		# 内插算法：直线、航位推算
+		master_eid = self.world.master_eid()
 		for eid, comp_tuple in component_tuples:
+			if eid == master_eid:
+				continue
 			comp_transform, = comp_tuple
